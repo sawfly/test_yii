4ddc227f029f023fc59306358f7b4a1e94ea82fc
eSawfly <eugene.sawfly@gmail.com>
2016-11-09 10:43:40 +0200

    users migration

---
+++ application/console/migrations/m161109_081342_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `users`.
+ */
+class m161109_081342_create_users_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('users', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(50),
+            'password' => $this->string(),
+            'email' => $this->string(),
+            'status_id' => $this->smallInteger()->unsigned()->defaultValue(0),
+            'permission_id' => $this->smallInteger()->unsigned()->defaultValue(0)
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('users');
+    }
+}
